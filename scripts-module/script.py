3dc74a3a2392ccd876be118c58fba9765f454f78
Ivan Klivitskyi <vklivitskiy@gmail.com>
2025-01-20 13:41:23 +0100

    Vault-psql && script-module && handler

---
+++ scripts-module/script.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+
+print('Python sript')
+print(f'Date: {datetime.now()}')
---
+++ scripts-module/script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Bash script"
+echo "Date: $(date)"
